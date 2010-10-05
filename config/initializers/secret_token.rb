1b273bfc603bb2e935327b34a983f018522df997
Zach Ewert <zachanima@gmail.com>
2010-10-05 15:29:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yule::Application.config.secret_token = '72be04231e33fe57dc760a27226944e734ac5668face0d468f9d3cfcbd7898b31b11de249c3084d50ed49a25f63ee333dcd120ed64e873ac92a7091b82fac9e6'
