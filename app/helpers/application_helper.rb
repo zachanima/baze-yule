582fb588b402396273ff437a5aba83cbc6e3618e
Zach Ewert <zachanima@gmail.com>
2010-10-07 01:20:56 +0200

    Add tooltip method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def tooltip(content, text = '&hellip;', type = nil)
+    content = html_escape content unless content.html_safe? 
+    text    = html_escape text    unless text.html_safe? 
+    ['<span class="tooltip">', text, case type
+      when :img then ['<img src="', content, '" />'].join
+      else           "<div>#{content}</div>"
+    end, '</span>'].join.html_safe
+  end
 end
