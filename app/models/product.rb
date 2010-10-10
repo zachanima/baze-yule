ac2c6b7202d25aaba0cba81ca2766dcfdcf713b9
Zach Ewert <zachanima@gmail.com>
2010-10-10 19:24:49 +0200

    Add medium style to image in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ActiveRecord::Base
   has_many :option_groups
   validates_presence_of :name
 
-  has_attached_file :image, :styles => { :original => '800x800>', :thumb => '200x200>' }
+  has_attached_file :image, :styles => { :original => '800x800>', :medium => '300x300>', :thumb => '200x200>' }
 end
