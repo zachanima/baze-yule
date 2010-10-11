f5a0a78a05ed1691a4381a96906828e9863c1a7c
Zach Ewert <zachanima@gmail.com>
2010-10-11 06:34:16 +0200

    Set locale for devise controller in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :sort_direction
   before_filter :find_shop
+  before_filter :set_locale
   layout :layout_by_resource
 
   def layout_by_resource
     devise_controller? ? 'shop' : 'application'
+  end
+
+  def set_locale
+    if devise_controller?
+      I18n.locale = @shop.locale
+    end
   end
 
   private
