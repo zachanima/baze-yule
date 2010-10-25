bed09fb55baff57505a9ccea3571fe112f37c63a
Zach Ewert <zachanima@gmail.com>
2010-10-25 07:48:05 +0200

    Require product shop id in form, model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :shop
   has_many :option_groups
   has_many :orders
-  validates_presence_of :name
+  validates_presence_of :name, :shop_id
 
   has_attached_file :image, :styles => { :original => '800x800>', :medium => '300x300>', :thumb => '200x200>' }
 end
